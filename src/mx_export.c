a42bda83fa5fb386943d0f3de100808ef3a19509
Yurii Makohon <ymakohon@e2r7p3.unit.ua>
2020-01-28 18:48:31 +0200

    fix mx_match.c, now it working done

--- src/mx_export.c
+++ src/mx_export.c
@@ -1,37 +1,60 @@
 #include "ush.h"
 
-static char *get_var_name(char *str);
-static void get_value(char **value, char *str, int space_index);
+static void print_env();
+// static char *get_var_name(char *str);
 
-void mx_export(char *str) {
-    char **strs = mx_strsplit(str, ' '); 
-    bool flag = 0;
-
-    for (int i = 0; strs[i]; i++) {
-        char *value = mx_strdup("");
-        int space_index = mx_get_char_index(strs[i], '=');
-        char *env_var_name = get_var_name(strs[i]);
-
-        if (strs[i][0] == '=' && !flag) {
-            fprintf(stderr, "ush: bad assignment");
-            flag = 1;
+int mx_export(char **args) {
+    if (args[0] == NULL)
+        print_env();
+    else {
+        for (int i = 0; args[i]; i++) {
+            if (mx_match(args[i], MX_EXPORT_ARG))
+                printf("%s\n", args[i]);
+            else
+                printf("ERROR: %s\n", args[i]);
         }
-        if (space_index != -1 && !flag)
-            get_value(&value, strs[i], space_index);
-        setenv(env_var_name, value, 1);
-        mx_strdel(&env_var_name);
-        mx_strdel(&value);
     }
-    mx_del_strarr(&strs);
+    return 0;
 }
 
-static void get_value(char **value, char *str, int space_index) {
-    mx_strdel(value);
-    *value = mx_strdup(str += space_index + 1);
+static void print_env() {
+    extern char **environ;
+    int len = 0;
+
+    for (int i = 0; environ[i]; i++)
+        len++;
+    mx_bubble_sort(environ, len);
+    mx_print_strarr(environ, "\n");
 }
 
-static char *get_var_name(char *str) {
-    char *name = mx_strndup(str, mx_get_char_index(str, '='));
+// static char *get_var_name(char *str) {
+//     char *name = mx_strndup(str, mx_get_char_index(str, '='));
 
-    return name;
-}
+//     return name;
+// }
+// static int test()
+// {
+//     reti = regcomp(&regex, MX_EXPORT_ARG, 0);
+//     if (reti)
+//     {
+//         fprintf(stderr, "Could not compile regex\n");
+//         exit(1);
+//     }
+
+//     reti = regexec(&regex, "A=a", 0, NULL, 0);
+//     if (!reti)
+//     {
+//         puts("Match");
+//     }
+//     else if (reti == REG_NOMATCH)
+//     {
+//         puts("No match");
+//     }
+//     else
+//     {
+//         regerror(reti, &regex, msgbuf, sizeof(msgbuf));
+//         fprintf(stderr, "Regex match failed: %s\n", msgbuf);
+//         exit(1);
+//     }
+//     return 0;
+// }
