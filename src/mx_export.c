d9c30133df679ad1b5dedc2f98180fbef74505b7
Yurii Makohon <ymakohon@e2r7p3.unit.ua>
2020-01-29 19:35:29 +0200

    updated mx_export & mx_pwd func

--- src/mx_export.c
+++ src/mx_export.c
@@ -2,33 +2,52 @@
 
 static char *get_var_name(char *str);
 static char *get_var_val(char *arg);
+static bool parse_error(char *arg);
 
-int mx_export(char **args) {
-    if (args[0] == NULL)
+int mx_export(char **args)
+{
+    bool args_stop = 0;
+
+    if (args[0] == NULL) // MAYBE BUG WHEN "-" flag
         mx_print_env();
-    else {
-        for (int i = 0; args[i]; i++) {
+    else
+        for (int i = 0; args[i] && !args_stop; i++)
             if (mx_match(args[i], MX_EXPORT_ARG))
-                if (mx_match(args[i], MX_ENV_ARG)) {
+                if (mx_match(args[i], "="))
+                {
                     setenv(get_var_name(args[i]), get_var_val(args[i]), 1);
                     // And add this Variable to own export list
                 }
                 else
-                    printf("ONLY FOR EXPORT\n"); // Add only to own export list
+                    printf("ONLY FOR EXPORT\n"); // Add only to export list
             else
-                printf("ERROR: %s\n", args[i]); // Pars Errors!
-        }
-    }
+                args_stop = parse_error(args[i]);
     return 0;
 }
 
-static char *get_var_name(char *arg) {
+static bool parse_error(char *arg)
+{
+    if (mx_match(arg, "^[-+]"))
+        fprintf(stderr, "export: does not accept any options: %c%c\n",
+                arg[0], arg[1]);
+    else if (mx_match(arg, "^[0-9]"))
+        fprintf(stderr, "export: not an identifier: %s\n",
+                get_var_name(arg));
+    else
+        fprintf(stderr, "export: not valid in this context: %s\n",
+                get_var_name(arg));
+    return 1;
+}
+
+static char *get_var_name(char *arg)
+{
     char *name = mx_strndup(arg, mx_get_char_index(arg, '='));
 
     return name;
 }
 
-static char *get_var_val(char *arg) {
+static char *get_var_val(char *arg)
+{
     char *value = mx_strdup(arg + mx_get_char_index(arg, '=') + 1);
 
     return value;
