e64deb6bcfa1cea1eec764e76d58431c5e83b629
Yurii Makohon <ymakohon@e2r7p3.unit.ua>
2020-01-29 12:47:56 +0200

    commit for pull

--- inc/ush.h
+++ inc/ush.h
@@ -18,13 +18,16 @@
 #define MX_SHELL_NAME "ush"
 #define MX_SHELL_PROMPT "u$h"
 #define MX_NON_PRINTABLE "[\x03\x0a]"
+#define MX_EXPORT_ARG "^[A-Za-z_]+[A-Za-z0-9_]*=?[A-Za-z0-9_]*$"
+#define MX_ENV_ARG "^[A-Za-z_]+[A-Za-z0-9_]*="
 
 void mx_get_input(char *buf, int *code);
 struct termios *mx_get_tty();
 void mx_enable_canon();
 void mx_disable_canon();
 bool mx_match(char *src, char *regex);
+void mx_print_env();
 
 int mx_unset(char *name);
-void mx_export(char *str);
+int mx_export(char **args);
 int mx_pwd(char **flags);
