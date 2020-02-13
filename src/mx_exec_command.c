3a2dde8dadc99857ac8569e5660f6fbebb26752c
Yurii Makohon <ymakohon@e2r7p3.unit.ua>
2020-02-13 16:05:57 +0200

    fix \'++(argv)\' to \'&argv[1]\'

--- src/mx_exec_command.c
+++ src/mx_exec_command.c
@@ -48,19 +48,19 @@ static int exec_our(char *command, char **argv, int fd) {
     pid_t pid;
 
     if (strcmp(command, "unset") == 0)
-        return mx_unset(++(argv));
+        return mx_unset(&argv[1]);
     if (strcmp(command, "export") == 0)
-        return mx_export(++(argv), fd);
+        return mx_export(&argv[1], fd);
     pid = fork();
     if (pid == 0) {
         if (strcmp(command, "pwd") == 0)
-            exit(mx_pwd(++(argv), fd));
+            exit(mx_pwd(&argv[1], fd));
         if (strcmp(command, "cd") == 0)
-            exit(mx_cd(++(argv)));
+            exit(mx_cd(&argv[1]));
         if (strcmp(command, "echo") == 0)
-            exit(mx_cd(++(argv)));
+            exit(mx_cd(&argv[1]));
         if (strcmp(command, "which") == 0)
-            exit(mx_which(++(argv), fd));
+            exit(mx_which(&argv[1], fd));
     }
     return -1;
 }
