3cf8dcd417302b499c6a6ce75035c2201372e443
okhomin <okhomin@e2r6p4.unit.ua>
2020-02-18 14:52:14 +0200

    removed new parser

--- Makefile
+++ Makefile
@@ -9,7 +9,6 @@ LMBX = libmx.a
 INC = ush.h
 SRC = mx_ush.c \
 	mx_check_chars.c \
-	mx_replace_sub.c \
 	mx_replace_substitution.c \
 	mx_split_command.c \
 	mx_replace_tilde.c \
@@ -82,6 +81,7 @@ SRC = mx_ush.c \
 	mx_clearenv.c \
 	mx_env_fill.c \
 	mx_print_env_error.c \
+	mx_replace_sub.c \
 	# mx_env.c \
 
 INCLUDE = -I $(LBMXD) \
--- src/mx_ush.c
+++ src/mx_ush.c
@@ -1,7 +1,5 @@
 #include "ush.h"
-// #include <paths.h>
 
-// static void test();
 static void main_cycle();
 static void init();
 static void deinit();
@@ -13,29 +11,11 @@ int main(int argc, char **argv) {
         return 1;
     }
     init();
-    // test();
     main_cycle();
     deinit();
-    // setvbuf(stdout, NULL, _IONBF, 0);
-    // char *argv[20] = {"-e", "-ne\x0a", NULL};
-    // mx_echo(argv);
     system("leaks -q ush");
     return 0;
 }
-
-    
-// static void test() {
-//     wordexp_t p;
-//     char **w;
-//     size_t i;
-
-//    int result = wordexp("echo `ls; lsf`", &p, WRDE_SHOWERR);
-//     w = p.we_wordv;
-//     for (i = 0; i < p.we_wordc; i++)
-//         printf("%s\n", w[i]);
-//     printf("code = %d\n", result);
-//     wordfree(&p);
-// }
 
 static void main_cycle() {
     int result = 0;
@@ -45,6 +25,7 @@ static void main_cycle() {
     while (strcmp(prompt->command, "exit") && result != -1) {
         mx_get_input(prompt, &result);
         printf("\r\n");
+
         mx_handle_command(prompt->command, &result);
     }
     mx_d_del_list(&prompt->history_head);
@@ -54,21 +35,18 @@ static void main_cycle() {
 
 static void deinit() {
     mx_disable_canon();
-    // TODO
     t_map **map = mx_get_lenv();
     mx_del_map(map);
-    //
 }
 
 static void init() {
-    // TODO
     mx_init_var_lists();
     t_map **map = mx_get_lenv();
     char path[PATH_MAX];
     *map = mx_create_map(40);
 
     getcwd(path, sizeof(path));
-    mx_put_map(map, strdup("OLDPWD"), strdup(getenv("OLDPWD"))); //NULL if first pwd
+    mx_put_map(map, strdup("OLDPWD"), strdup(getenv("OLDPWD")));
     mx_put_map(map, strdup("PWD"), strdup(getenv("PWD")));
     mx_put_map(map, strdup("?"), mx_itoa(0));
     mx_put_map(map, strdup("#"), mx_itoa(0));
@@ -77,13 +55,9 @@ static void init() {
     mx_put_map(map, strdup("$"), mx_itoa(getpid()));
     mx_put_map(map, strdup("*"), strdup(""));
     mx_put_map(map, strdup("@"), strdup(""));
-    // sigset_t mask;
-    // sigfillset(&mask);
-    // sigprocmask(SIG_SETMASK, &mask, NULL);
-    //
-    // setenv("MX_PROMPT", "u$h> ", 0);
-    // mx_var_list_insert(SHELL, "MX_PROMPT=u$h> ");
-    // mx_var_list_insert(EXP, "MX_PROMPT=u$h> ");
+    sigset_t mask;
+    sigfillset(&mask);
+    sigprocmask(SIG_SETMASK, &mask, NULL);
     char *args[3] = {"MX_PROMPT=ush> ", NULL};
     mx_export(args, 1);
     tcgetattr(STDIN_FILENO, mx_get_tty());
