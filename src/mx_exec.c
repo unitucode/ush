d7a91e55f36177f8b08ce811561897f015f7e768
okhomin <okhomin@e2r6p4.unit.ua>
2020-02-19 18:02:10 +0200

    added position

--- src/mx_exec.c
+++ src/mx_exec.c
@@ -4,13 +4,13 @@ int mx_exec(t_process *process, char *filename, char **argv, char **env) {
     t_list **list = mx_get_list_procs();
     int retval = 0;
 
-    mx_disable_canon();
     process->command = mx_arr_to_str(argv);
+    mx_disable_canon();
     process->status = posix_spawn(&process->pid, filename,
                          &process->actions, &process->attrs, argv, env);
     if (waitpid(process->pid, &process->status, WUNTRACED) != -1) {
         if (MX_WIFSTOPPED(process->status)) {
-            if ((*list)->data)
+            if (*list && (*list)->data)
                 process->pos = ((t_process *)(*list)->data)->pos + 1;
             mx_push_back(list, process);
         }
