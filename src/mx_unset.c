a0287e66c930065a715b1f25e0bc1ccfd043a1fe
Yurii Makohon <ymakohon@e2r7p3.unit.ua>
2020-02-25 14:54:53 +0200

    added -e -a flags to unset

--- src/mx_unset.c
+++ src/mx_unset.c
@@ -27,28 +27,40 @@ static void unset_all(void) {
         mx_strdel(&var);
 }
 
-static int unset_parse_flag(char *flag) {
-    if (!mx_match(flag, "^-[ea]+")) {
-        fprintf(stderr, "unset: invalid option\n");
-        fprintf(stderr, "usage: unset [-ea] var ...\n");
-        return 1;
+static int parse_flags(char **flags, int *idx, int *mode) {
+    for (*idx = 0; flags[*idx] && flags[*idx][0] == '-'
+         && strcmp(flags[*idx], "--") != 0;) {
+        if (!mx_match(flags[*idx], "^-[ea]+$")) {
+            fprintf(stderr, "unset: invalid option\n");
+            fprintf(stderr, "usage: unset [-ea] var ...\n");
+            return 1;
+        }
+        if (mx_match(flags[*idx], "a"))
+            *mode = 2;
+        *mode = *mode == 2 ? 2 : 1;
+        (*idx)++;
     }
-    if (mx_match(flag, "a"))
-        unset_all();
-    mx_clearenv();
+    if (flags[*idx] && !strcmp(flags[*idx], "--"))
+        (*idx)++;
     return 0;
 }
 
 int mx_unset(char **args) {
     int result = 0;
+    int idx;
+    int mode = 0;
 
-    for (int i = 0; args[i]; i++) {
-        if (args[i][0] == '-')
-            result = unset_parse_flag(args[i]);
-        else if (unset_var(args[i]) == -1 && !result) {
-            fprintf(stderr, "unset: %s: invalid parameter name\n", args[i]);
+    result = parse_flags(args, &idx, &mode);
+    for (; args[idx] && result != 1; idx++) {
+        if (unset_var(args[idx]) == -1 && !result) {
+            fprintf(stderr, "unset: %s: invalid parameter name\n", args[idx]);
             result = 1;
         }
+    }
+    if (mode && result != 1) {
+        if (mode == 2)
+            unset_all();
+        mx_clearenv();
     }
     return result;
 }
