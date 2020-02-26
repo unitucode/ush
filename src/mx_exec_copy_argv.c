756e10e9063d7f5b31c257b3b2cb59efdea95b25
okhomin <okhomin@e2r6p4.unit.ua>
2020-02-26 12:36:21 +0200

    fixed auditor

--- src/mx_exec_copy_argv.c
+++ src/mx_exec_copy_argv.c
@@ -2,7 +2,7 @@
 
 char **mx_exec_copy_argv(char **argv) {
     size_t size = mx_arr_size(argv);
-    char **copy = malloc(sizeof(char *) * (size + 1));
+    char **copy = malloc(sizeof(char*) * (size + 1));
 
     copy[size] = NULL;
     for (unsigned int i = 0; i < size; i++) {
