d23a6fe0bec80e22cd4ce02c9ecbbe012e2bb0b7
Yurii Makohon <ymakohon@e2r7p3.unit.ua>
2020-02-26 12:35:24 +0200

    fix Auditor mx_check_substitution.c

--- src/mx_check_substitutions.c
+++ src/mx_check_substitutions.c
@@ -3,7 +3,7 @@
 bool mx_check_substitutions(char *command) {
     bool g_quotes = false;
     size_t len = strlen(command);
-    
+
     for (unsigned int i = 0; i < len; i++) {
         mx_skip_quotes(command, &i, MX_S_QUOTES);
         mx_skip_expansion(command, &i);
