724596e1838b3cb01b020009156d7219cd77fdbf
okhomin <okhomin@okhomins-iMac.local>
2020-02-27 23:50:31 +0200

    new hash func in hashfile

--- src/mx_hashfile.c
+++ src/mx_hashfile.c
@@ -12,24 +12,31 @@ static bool check_args(char **args) {
     return true;
 }
 
-int mx_hashfile(char **args, int fd) {
+static void print_hash(char *file, int fd) {
     int hash = 0;
+
+    hash = mx_hash(file);
+    for (int i = 0; i < 10; i++) {
+        dprintf(fd, "%x", (hash + 42) % 99999999);
+        hash /= 2;
+    }
+    dprintf(fd, "\n");
+}
+
+int mx_hashfile(char **args, int fd) {
     char *file = NULL;
-    char *hex = NULL;
 
     if (!check_args(args))
         return 1;
     file = mx_file_to_str(args[0]);
     if (file) {
-        hash = mx_hash(file);
-        for (int i = 0; i < 4; i++) {
-            hex = mx_nbr_to_hex(hash / (i + 1));
-            dprintf(fd, "%s", hex);
-            mx_strdel(&hex);
-        }
-        printf("\n");
+        print_hash(file, fd);
         mx_strdel(&file);
         return 0;
+    }
+    else if (!strlen(file)) {
+        fprintf(stderr, "hashfile: file is empty\n");
+        return 1;
     }
     fprintf(stderr, "hashfile: %s\n", strerror(errno));
     return 1;
