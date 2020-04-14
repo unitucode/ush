667522d440d20af91955209da2d87c8b459a2daa
okhomin <okhomin@e2r2p3.unit.ua>
2020-04-15 02:29:35 +0300

    changed canon mode

--- src/mx_enable_canon.c
+++ src/mx_enable_canon.c
@@ -6,7 +6,5 @@ void mx_enable_canon(void) {
     tcgetattr(STDIN_FILENO, mx_get_tty());
     tty = *mx_get_tty();
     tty.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    tty.c_cc[VMIN] = 1;
-    tty.c_cc[VTIME] = 0;
     tcsetattr(STDIN_FILENO, TCSADRAIN, &tty);
 }
