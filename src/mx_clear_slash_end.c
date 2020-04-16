22867ccebebd4f4a6e2495c3d76566b91b2036a8
Maksym Avramenko <mavramenko@e2r11p14.unit.ua>
2020-04-16 13:11:52 +0300

    fixed bug with slash

--- src/mx_clear_slash_end.c
+++ src/mx_clear_slash_end.c
@@ -1,7 +1,7 @@
 #include "ush.h"
 
 char *mx_clear_slash_end(char *str) {
-    if (str[mx_strlen(str) - 1] == '/') {
+    if (str[mx_strlen(str) - 1] == '/' && mx_strlen(str) > 1) {
         char *result = mx_strndup(str, mx_strlen(str) - 1);
 
         mx_strdel(&str);
